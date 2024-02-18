0c5c261384cccd4166823d87ec71b345cfef2125
saracodingx <160379800+saracodingx@users.noreply.github.com>
2024-02-18 13:06:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,34 @@
 function setup() {
-  createCanvas(830, 1500);
+  createCanvas(830,1500);
+
   background("#000BD0");
+  
 }
 
 function draw() {
-  fill(255);
-  stroke(0);
-  strokeWeight(3);
   
-  if (mouseIsPressed) {
-    rect(mouseX, mouseY, 100, 60);
-    textSize(determineFontSize());
-    text("ERROR 404: YOUR PRIVACY NOT FOUND", width / 2, height / 3);
-  }
-}
+  
+  //fill(0);
+  fill(255);
+  //noStroke(0);
+  stroke(0);
+  strokeWeight(4);
+ 
+  
+  
+  if(mouseIsPressed == true)
+    {
+      
+      rect(mouseX,mouseY,100,60);
+      //rect(mouseX,mouseY,random(100,30) );
+      //circle(mouseX,mouseY,90);
+    //text("ERROR: YOUR PRIVACY NOT FOUND",width/2.3,height/2.3)
+      textSize(30);
+      //xt("ERROR: YOUR PRIVACY NOT FOUND",width/2.3,height/2.1);
+    text("ERROR 404: YOUR PRIVACY NOT FOUND",width/3,height/4);
+                                            
+    }
 
-function determineFontSize() {
-  // Determine appropriate font size based on canvas dimensions
-  let fontSize;
-  if (width > height) { // Landscape orientation (Desktop)
-    fontSize = width / 50; // Adjust the division factor to suit your preference
-  } else { // Portrait orientation (Mobile)
-    fontSize = height / 20; // Adjust the division factor to suit your preference
-  }
-  return fontSize;
+
+
 }
