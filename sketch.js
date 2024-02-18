a5a2ba0b43b5ad7d7a0d6df4ed2f62b4d2ca99c7
saracodingx <160379800+saracodingx@users.noreply.github.com>
2024-02-18 13:53:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function draw() {
   if(mouseIsPressed == true)
     {
       
-      rect(mouseX,mouseY,200,145);
+      rect(mouseX,mouseY,200,120);
       //rect(mouseX,mouseY,random(100,30) );
       //circle(mouseX,mouseY,90);
     //text("ERROR: YOUR PRIVACY NOT FOUND",width/2.3,height/2.3)
